087870456f2dc964b61f8b7489576287a0b5d646
Emilcar <e2434741@cmaisonneuve.qc.ca>
2026-09-16 10:07:48 -0400

    affichage totale

---
+++ observateurs/Affichage_Totale.py
@@ -0,0 +1,13 @@
+
+from observateurs import observateur
+
+class Affichage_Total(observateur):
+    def _init_(self, sujet):
+    #TODO : abonner l'observateur au sujet
+        pass
+
+
+    def actualiser(self, sujet):
+        #TODO : récupérer les données(getdonnes) du sujet et mettre à jour l'affichage du total
+        #calculer le total des articles et mettre à jour le label_total
+        pass 
